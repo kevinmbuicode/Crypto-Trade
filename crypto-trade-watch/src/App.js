6644e6bcd616ecb64e522395a80efef1c0dda8b5
kevinmbuicode <kevinmushan@gmail.com>
2022-08-06 14:33:41 +0300

    Downgraded to React 17 to use mui/styles

--- crypto-trade-watch/src/App.js
+++ crypto-trade-watch/src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import CoinPage from './Pages/CoinPage';
+import HomePage from './Pages/HomePage';
 
 function App() {
+  
+
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/coins/:id' element={<CoinPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
