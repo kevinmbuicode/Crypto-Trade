91fe140506b6f24f6ed31bfa475f50db5a44a6f0
kevinmbuicode <kevinmushan@gmail.com>
2022-08-09 23:24:01 +0300

    context api before branching

--- crypto-trade-watch/src/App.js
+++ crypto-trade-watch/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
@@ -13,6 +14,32 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage/>}/>
           <Route path='/coins/:id' element={<CoinsPage/>}/>
+=======
+import { makeStyles } from '@mui/styles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import CoinPage from './Pages/CoinPage';
+import HomePage from './Pages/HomePage';
+
+function App() {
+  const useStyles = makeStyles({
+    App: {
+      backgroundColor: '#14161a',
+      color: 'white',
+    }
+  })
+
+  const classes = useStyles()
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/coins/:id' element={<CoinPage/>}/>
+>>>>>>> 348dabd56a645a7d0b784cd61b9e4961b8b4a980
         </Routes>
       </div>
     </BrowserRouter>
